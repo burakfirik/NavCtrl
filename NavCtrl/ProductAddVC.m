25dadffd1958d0d448859157e32508493ff387ef
burak <burakfirik@gmail.com>
2018-01-05 16:26:34 -0500

    Add Products to Companies

---
+++ NavCtrl/ProductAddVC.m
@@ -0,0 +1,37 @@
+//
+//  ProductAddVC.m
+//  NavCtrl
+//
+//  Created by Aditya Narayan on 1/5/18.
+//  Copyright © 2018 Aditya Narayan. All rights reserved.
+//
+
+#import "ProductAddVC.h"
+
+@interface ProductAddVC ()
+
+@end
+
+@implementation ProductAddVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
