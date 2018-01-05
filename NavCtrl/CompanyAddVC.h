af552ad804a136d708ed6456f09938f0e042aff2
burak <burakfirik@gmail.com>
2018-01-05 15:42:58 -0500

    add companyVC with programmatic autolayout

---
+++ NavCtrl/CompanyAddVC.h
@@ -0,0 +1,13 @@
+//
+//  CompanyAddVC.h
+//  NavCtrl
+//
+//  Created by Aditya Narayan on 1/5/18.
+//  Copyright © 2018 Aditya Narayan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CompanyAddVC : UIViewController
+
+@end
